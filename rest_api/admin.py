5e917b1b56a62d86d44b9aee99fa2ea46eebb059
Apple <apple@MacBook-Pro-4.local>
2024-06-27 22:30:55 -0400

    Add Anotation type table

--- rest_api/admin.py
+++ rest_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(ImageInfo)
+admin.site.register(AnnotationType)
+admin.site.register(AnnotationSetup)
+admin.site.register(ObjectClass)
+admin.site.register(Polygons)
--- rest_api/apps.py
+++ rest_api/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class RestApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "rest_api"
+
+    def ready(self):
+        from .dbhelper import add_annotation_type_if_not_exist
+        add_annotation_type_if_not_exist()
